cbd2d872c5081fdd65797bec7f69f35436fc618c
xakunamatata <marta.aladova@gmail.com>
2020-04-11 01:04:38 +0300

    before finishing touches

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ $('.nav-burger').click(function(){
     $(this).toggleClass('burger-close');
     $('.nav-menu').toggleClass('nav-menu-open');
 })
+
+// gallery
+
+$('#portfolio').lightGallery({
+    thumbnail:true,
+    animateThumb: false,
+    showThumbByDefault: false
+}); 
+
